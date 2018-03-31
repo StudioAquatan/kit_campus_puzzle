a2c2daa4d10e311dcadb46d976ce89a0419807ed
Pudding <s.kokuryo@gmail.com>
2018-03-31 23:11:42 +0900

    fix #14 fix redirect link

--- src/kit_campus_puzzle/puzzle/mixin.py
+++ src/kit_campus_puzzle/puzzle/mixin.py
@@ -4,7 +4,7 @@ from django.utils.decorators import method_decorator
 
 
 class LoginRequiredMixin(object):
-    @method_decorator(login_required)
+    @method_decorator(login_required(login_url='/accounts/twitter/login'))
     def dispatch(self, request, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
 
